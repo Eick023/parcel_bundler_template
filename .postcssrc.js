071355def253fecc4407b3f7c239529b2848cee3
Eick023 <ya5819@naver.com>
2024-04-24 15:51:33 +0900

    parcel bundler

---
+++ .postcssrc.js
@@ -0,0 +1,15 @@
+// common.js - node.js
+const autoprefixer = require('autoprefixer');
+module.exports = {
+  plugins : [
+    autoprefixer
+  ]
+};
+
+// esm -> javascript(vanille)
+// import auto from 'autoprefixer';
+// export {
+//   plugins : [
+//     auto
+//   ]
+// };
